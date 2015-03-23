e58d14ee159a5b91d0e108308a156c123566e51b
Cristiano Carvalheiro <ei10041@fe.up.pt>
2015-03-23 12:51:22 +0000

    added gitignore

---
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByBoundary : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider other) {
+		// should look for 'lemming' tag instead, need to update prefabs
+		if (other.tag != "Terrain" && other.tag != "Hover") {
+			Destroy(other.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject[] lemmings;
+
+	void Start () {
+		GameObject lemming = GetRandomLemming();
+		Instantiate(lemming, new Vector3 (0.0f, 0.0f, 0.0f), Quaternion.identity);
+	}
+
+	public GameObject GetRandomLemming() {
+		return lemmings[Random.Range(0, lemmings.Length)];
+	}
+}
