541798a6be3e21e99b22a6e81a74bf7271dad226
Cristiano Carvalheiro <ei10041@fe.up.pt>
2015-03-26 10:35:13 +0000

    minor changes

--- Assets/Scripts/Menu/GameEndController.cs
+++ Assets/Scripts/Menu/GameEndController.cs
@@ -1,10 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameEndController : MonoBehaviour {
 
+	public Text resultText;
+	public Text scoreText;
+
+	public Button next;
+	public Button retry;
+
+	public Button menu;
+	public Button exit;
+
+	private Animator animtr;
+
 	void Awake() {
-		gameObject.SetActive (true);
+		animtr.GetComponent<Animator> ();
+
+		gameObject.SetActive (false);
 	}
 
 	// Use this for initialization
@@ -14,6 +28,24 @@ public class GameEndController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//if(GameController.lemmingsLeft <= 0 && GameObject.FindWithTag("Lemming") == null) {
+
+		//}
+	}
+
+	public void NextLevel() {
+		// Application.LoadLevel (Application.loadedLevelName);
+	}
+
+	public void Retry() {
+		Application.LoadLevel (Application.loadedLevelName);
+	}
+
+	public void MainMenu() {
+		Application.LoadLevel("MainMenu");
+	}
+
+	public void Exit() {
+		Application.Quit ();
 	}
 }
