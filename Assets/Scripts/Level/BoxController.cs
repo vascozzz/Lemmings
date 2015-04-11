fab967d22ad311a425d0bc351be39070a1a3d9fc
Vasco Gomes <ovascogomes@gmail.com>
2015-04-11 19:39:12 +0100

    Added two new obstacles types, will finish later tonight
    Obstacles models folder is a mess atm, should be looked at later.

---
+++ Assets/Scripts/Level/BoxController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxController : MonoBehaviour 
+{
+	// public
+	public float speed;
+	public GameObject[] obstacleTypes;
+
+	// private
+	private Vector3 target;
+	private Vector3 targetVector;
+	private bool reachedTarget;
+
+	void Start() {
+		reachedTarget = false;
+	}
+
+	void FixedUpdate() {
+		if (!reachedTarget) {
+			GetComponent<Rigidbody>().velocity = targetVector * speed;
+		} 
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Terrain") {
+			Destroy(this.gameObject);
+
+			GameObject newType = obstacleTypes[Random.Range(0, obstacleTypes.Length)];
+			GameObject newObstacle = Instantiate(newType) as GameObject;
+
+			newObstacle.transform.position = new Vector3(target.x, 0.0f, target.z); // once we're at the correct position, we should get rid of the y offset
+			//newObstacle.GetComponent<TrampolineController>().SetTarget(target);
+		}
+	}
+
+	public void SetTarget(Vector3 target, Vector3 targetVector) {
+		this.target = target;
+		this.targetVector = targetVector;
+	}
+}
---
+++ Assets/Scripts/Level/TrampolineController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrampolineController : MonoBehaviour 
+{
+	public float jumpForce;
+
+	private Vector3 target;
+
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Lemming") {
+			other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(-1.0f, 1.0f, 0.0f) * jumpForce);
+
+			//TurretController.filledTiles.Remove(target);
+			//Destroy(this.gameObject);
+		}
+	}
+
+	public void SetTarget(Vector3 target) {
+		this.target = target;
+	}
+}
